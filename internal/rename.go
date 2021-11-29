99b0d1566ea8bce6ff11df016edc2bb127154149
ale8k <alexanderkilroy@outlook.com>
2021-11-29 17:47:23 +0000

    Exploring PPROF

--- internal/rename.go
+++ internal/rename.go
@@ -1,11 +1,10 @@
 package internal
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/segmentio/kafka-go"
@@ -32,34 +31,44 @@ func queryAllPages(dst []SOQuestionItem, currentPage int, startTime int) ([]SOQu
 	}
 }
 
-func parseQueryListToMessages(qryList []SOQuestionItem) []kafka.Message {
-	mapped := make([]kafka.Message, len(qryList))
+func parseQueryListToMessages(qryList []SOQuestionItem, mutex *sync.Mutex, ch chan<- kafka.Message, wg *sync.WaitGroup) {
+	mutex.Lock()
 	for i := 0; i < len(qryList); i++ {
 		data, _ := json.Marshal(qryList[i])
-		mapped[i] = kafka.Message{
+		ch <- kafka.Message{
 			Key:   []byte(strconv.Itoa(qryList[i].QuestionID)),
 			Value: data,
 			// Headers: []protocol.Header{},
 		}
 	}
-	return mapped
+	mutex.Unlock()
+	wg.Done()
 }
 
 func RenameMe(done chan struct{}) {
 	date, _ := time.Parse("01-02-2006", time.Now().Format("01-02-2006"))
 	finalDataQueryList := make([]SOQuestionItem, 0)
 	finalDataQueryList, err := queryAllPages(finalDataQueryList, 1, int(date.Unix()))
+
 	if err != nil {
 		// skip this push
 	}
-	fmt.Println(len(finalDataQueryList))
-	msgBatch := parseQueryListToMessages(finalDataQueryList)
 
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "kafka:9092", "test-topic", 0)
+	parsedMessages := make(chan kafka.Message, len(finalDataQueryList))
+	// send channel to parse query list and have it fill buffer
+	// split it up in divisions of 4 perhaps, idk, break slice down and copy
+	// it is crucial u copy alex otherwise race conditions, or wrap inside parse in mutex?
+	// mutex probably easily
+	var mutex *sync.Mutex
+	var wg *sync.WaitGroup
+	wg.Add(1)
+	parseQueryListToMessages(finalDataQueryList, mutex, parsedMessages, wg)
+	wg.Wait()
+	//conn, err := kafka.DialLeader(context.Background(), "tcp", "kafka:9092", "test-topic", 0)
 
-	bytesWritten, err := conn.WriteMessages(msgBatch...)
+	// bytesWritten, err := conn.WriteMessages(msgBatch...)
 
-	log.Println(bytesWritten)
+	// log.Println(bytesWritten)
 
 	// for {
 	// 	select {
--- internal/server.go
+++ internal/server.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/pprof"
 	"os"
+
 	"strconv"
 
 	"github.com/ale8k/language_usage_by_so/internal/handlers"
@@ -13,14 +15,27 @@ import (
 var Server *http.Server
 
 func StartServer() {
-	done := make(chan struct{})
-	go RenameMe(done)
+	// done := make(chan struct{})
+	//go RenameMe(done)
 	port, err := strconv.Atoi(os.Getenv("PORT"))
 	if err != nil {
 		port = 9000
 	}
+	pprofEndpoints, err := strconv.ParseBool(os.Getenv("PPROF_ENDPOINTS"))
+	if err != nil {
+		log.Printf("Failed to set PPROF_ENDPOINTS")
+		pprofEndpoints = false
+	}
 
 	mux := http.NewServeMux()
+
+	if pprofEndpoints {
+		mux.HandleFunc("/debug/pprof/", pprof.Index)
+		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	}
 
 	Server := &http.Server{
 		Addr:    fmt.Sprintf("%s:%s", "", strconv.Itoa(port)),
