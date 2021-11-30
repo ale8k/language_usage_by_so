57a2e8f24376e4e42f02ffb351e36038ed4075ac
ale8k <alexanderkilroy@outlook.com>
2021-11-30 18:39:03 +0000

    Cleanup a bit
    - Struct better, now we can create 'question processors' on the fly
    for a range of tags to a range of topics

--- internal/question_processor.go
+++ internal/question_processor.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"strconv"
@@ -10,13 +11,19 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
+type QuestionProcessor struct {
+	KafkaWriter  kafka.Writer
+	Tags         string
+	CollectEvery time.Duration
+}
+
 // Queries all available pages until !HasMore
 // uses a single slice to consistently append too, dynamically sizing
 // throughout each iteration. It is unlikely no questions are asked,
 // but in the event there are none this should go to zero value.
 // 	- "We could return the current page if we wanted to be clever I guess
 //	   but rate limiting on SE API prevents us. Oh well."
-func queryAllPages(dst []SOQuestionItem, currentPage int, startTime int, tags string) ([]SOQuestionItem, error) {
+func (qp *QuestionProcessor) queryAllPages(dst []SOQuestionItem, currentPage int, startTime int, tags string) ([]SOQuestionItem, error) {
 	data, err := GetCreatedQuestionsSync(currentPage, 100, startTime, tags)
 	if err != nil {
 		return nil, fmt.Errorf("something went wrong querying for statistics: %w", err)
@@ -25,7 +32,7 @@ func queryAllPages(dst []SOQuestionItem, currentPage int, startTime int, tags st
 	dst = append(dst, data.Items...)
 	if data.HasMore {
 		currentPage++
-		return queryAllPages(dst, currentPage, startTime, tags)
+		return qp.queryAllPages(dst, currentPage, startTime, tags)
 	} else {
 		return dst, nil
 	}
@@ -33,7 +40,7 @@ func queryAllPages(dst []SOQuestionItem, currentPage int, startTime int, tags st
 
 // Parses our SO items into kafka messages concurrency safe
 // Delivers result back to a buffered channel with size of initial slice of SO items
-func parseQueryListToMessages(qryList []SOQuestionItem, mutex *sync.Mutex, ch chan<- kafka.Message, wg *sync.WaitGroup) {
+func (qp *QuestionProcessor) parseQueryListToMessages(qryList []SOQuestionItem, mutex *sync.Mutex, ch chan<- kafka.Message, wg *sync.WaitGroup) {
 	mutex.Lock()
 	fmt.Println(len(qryList))
 	for i := 0; i < len(qryList); i++ {
@@ -53,7 +60,7 @@ func parseQueryListToMessages(qryList []SOQuestionItem, mutex *sync.Mutex, ch ch
 // specially with reflection regarding slices, was only able to reflect.Slice type find
 // and just wanted to move on honestly.
 // When generics are released in 1.18, will solve issues like this!
-func divideQuestions(initialSlice []SOQuestionItem, size int) ([][]SOQuestionItem, int) {
+func (qp *QuestionProcessor) divideQuestions(initialSlice []SOQuestionItem, size int) ([][]SOQuestionItem, int) {
 	final := make([][]SOQuestionItem, 0)
 	j := 0
 	finalIndices := 0
@@ -72,37 +79,58 @@ func divideQuestions(initialSlice []SOQuestionItem, size int) ([][]SOQuestionIte
 // Ideally need a cache layer to check previous 10 minute of messages
 // and to compare current id, answered and other relevant metadata worth updating
 // and pushing a new message on. For now we just push everything every 10 minutes.
-func ProcessAskedQuestions(done chan struct{}, tags string, collectEvery time.Duration) {
-	date, _ := time.Parse("01-02-2006", time.Now().Format("01-02-2006"))
-	finalDataQueryList := make([]SOQuestionItem, 0)
-	finalDataQueryList, err := queryAllPages(finalDataQueryList, 1, int(date.Unix()), tags)
-
-	if err != nil {
-		// skip this push
-	}
-
-	parsedMessages := make(chan kafka.Message, len(finalDataQueryList))
-
+func (qp *QuestionProcessor) ProcessAskedQuestions(done chan struct{}) {
+	tickaticka := time.NewTicker(qp.CollectEvery)
 	var mutex sync.Mutex
 	var wg sync.WaitGroup
 
-	spliced, size := divideQuestions(finalDataQueryList, 100)
-	fmt.Printf("Size of divided is: %d \n", size)
-	wg.Add(size)
-	for i := 0; i < size; i++ {
-		go parseQueryListToMessages(spliced[i], &mutex, parsedMessages, &wg)
-	}
-	wg.Wait()
-	fmt.Printf("Channel size: %v \n", len(parsedMessages))
+	// Wrapper to query consistently from now date
+	runQuery := func() ([]SOQuestionItem, error) {
+		date, _ := time.Parse("01-02-2006", time.Now().Format("01-02-2006"))
+		finalDataQueryList := make([]SOQuestionItem, 0)
+		finalDataQueryList, err := qp.queryAllPages(finalDataQueryList, 1, int(date.Unix()), qp.Tags)
 
-	//conn, err := kafka.DialLeader(context.Background(), "tcp", "kafka:9092", "test-topic", 0)
-	// bytesWritten, err := conn.WriteMessages(msgBatch...)
-	// for {
-	// 	select {
-	// 	case <-ticker.C:
-	// 		fmt.Println("TODO")
-	// 	case <-done:
-	// 		return
-	// 	}
-	// }
+		return finalDataQueryList, err
+	}
+
+	// Wrapper to process
+	processQuestionsToKafka := func() {
+		data, _ := runQuery()
+		parsedMessages := make(chan kafka.Message, len(data))
+		// TODO: Make batching size configurable alex
+		spliced, size := qp.divideQuestions(data, 100)
+
+		wg.Add(size)
+		for i := 0; i < size; i++ {
+			go qp.parseQueryListToMessages(spliced[i], &mutex, parsedMessages, &wg)
+		}
+		wg.Wait()
+
+		fmt.Printf("Channel size: %v \n", len(parsedMessages))
+
+		// When would we need to actually cancel a fire-and-forget write...?
+		msgSlice := make([]kafka.Message, 0)
+
+		for msg := range parsedMessages {
+			msgSlice = append(msgSlice, msg)
+		}
+
+		fmt.Printf("Messages to be sent size: %v\n", len(msgSlice))
+		err := qp.KafkaWriter.WriteMessages(context.TODO(), msgSlice...)
+		if err != nil {
+			fmt.Printf("error sending to kafka: %v\n", err)
+		}
+
+	}
+
+	processQuestionsToKafka()
+	for {
+		select {
+		case <-tickaticka.C:
+			processQuestionsToKafka()
+		case <-done:
+			return
+		}
+	}
+
 }
--- internal/server.go
+++ internal/server.go
@@ -11,13 +11,45 @@ import (
 	"strconv"
 
 	"github.com/ale8k/language_usage_by_so/internal/handlers"
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/compress"
 )
 
 var Server *http.Server
 
 func StartServer() {
 	done := make(chan struct{})
-	go ProcessAskedQuestions(done, "go", time.Duration(time.Minute*10))
+	phpQp := &QuestionProcessor{
+		KafkaWriter: kafka.Writer{
+			Addr:         kafka.TCP("kafka:9092"),
+			Topic:        "php-questions",
+			Balancer:     &kafka.LeastBytes{},
+			MaxAttempts:  30,
+			BatchSize:    5, // what if < 5 were asked?
+			BatchBytes:   1048576,
+			BatchTimeout: time.Duration(time.Second * 5), // we don't care that much honestly
+			ReadTimeout:  10,                             // what is this?
+			WriteTimeout: 10,
+			RequiredAcks: 1,
+			Async:        false,
+			Completion: func(km []kafka.Message, err error) {
+				if err != nil {
+					log.Printf("kafka failed message deliver: %v", err)
+				}
+			},
+			Compression: compress.Gzip,
+			Logger:      nil,
+			ErrorLogger: kafka.LoggerFunc(func(s string, i ...interface{}) {
+				log.SetPrefix("KAFKA: ")
+				log.Printf("failed, %s, see: %v", s, i)
+			}),
+			// Transport:   nil, create a kafka.Transport for TLS config
+		},
+		Tags:         "php",
+		CollectEvery: time.Duration(time.Minute * 10),
+	}
+	go phpQp.ProcessAskedQuestions(done)
+
 	port, err := strconv.Atoi(os.Getenv("PORT"))
 	if err != nil {
 		port = 9000
