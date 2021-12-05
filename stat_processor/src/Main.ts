a5dfed8e5bd9a87c90f29edb4df9cf8247dbc5a5
ale8k <alexanderkilroy@outlook.com>
2021-12-05 18:17:33 +0000

    remote debugging from container

--- stat_processor/src/Main.ts
+++ stat_processor/src/Main.ts
@@ -1,37 +1,44 @@
 import express from "express";
-import {Kafka} from "kafkajs";
-import promClient from "prom-client";
+// import {Kafka} from "kafkajs";
+// import promClient from "prom-client";
 
-const kafka = new Kafka({
-    clientId: "stat_processor",
-    brokers: ["127.0.0.1:9093"]
-})
+// let k = 100;
+// console.log(k);
 
-const producer = kafka.producer();
-const consumer = kafka.consumer({groupId:  "stat_processor"});
+// const kafka = new Kafka({
+//     clientId: "stat_processor",
+//     brokers: ["127.0.0.1:9093"]
+// })
 
-const run = async () => {
-    await producer.connect();
+// const producer = kafka.producer();
+// const consumer = kafka.consumer({groupId:  "stat_processor"});
 
-    await consumer.connect()
-    await consumer.subscribe({topic: "python-questions", fromBeginning: true})
+// const run = async () => {
+//     await producer.connect();
 
-    await consumer.run({
-        eachMessage: async (message) => {
-            console.log(message)
-        }
-    })
-}
+//     await consumer.connect()
+//     await consumer.subscribe({topic: "python-questions", fromBeginning: true})
+
+//     await consumer.run({
+//         eachMessage: async (message) => {
+//             console.log(message)
+//         }
+//     })
+// }
 
 
 const app =  express();
 
 app.get("/main", (req, res) => {
-    run()
+    // run()
+    let k = 10;
+    let b = 20;
+    
     res.send({message: "Connected to main route"});
 })
 
 
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT)
+console.log("running on some port bro")
