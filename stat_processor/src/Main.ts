5f7abc3d623f8a120adf7d0ee00569a32bce52dc
Shreyas Balachandran <shreyas.bba@gmail.com>
2021-12-05 22:41:23 +0530

    stat_processor - Installed Express, Kafkajs and prom-client - Initialized Dockerfile for dev environment - Created basic express app, connects to Kafka instance and console.logs the topics.

--- stat_processor/src/Main.ts
+++ stat_processor/src/Main.ts
@@ -1,3 +1,37 @@
-console.log("yolo man");
+import express from "express";
+import {Kafka} from "kafkajs";
+import promClient from "prom-client";
 
-function n() { console.log("hi") } 
+const kafka = new Kafka({
+    clientId: "stat_processor",
+    brokers: ["127.0.0.1:9093"]
+})
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({groupId:  "stat_processor"});
+
+const run = async () => {
+    await producer.connect();
+
+    await consumer.connect()
+    await consumer.subscribe({topic: "python-questions", fromBeginning: true})
+
+    await consumer.run({
+        eachMessage: async (message) => {
+            console.log(message)
+        }
+    })
+}
+
+
+const app =  express();
+
+app.get("/main", (req, res) => {
+    run()
+    res.send({message: "Connected to main route"});
+})
+
+
+const PORT = process.env.PORT || 3100;
+
+app.listen(PORT)
